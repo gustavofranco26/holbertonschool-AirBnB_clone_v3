1cf6cfe8ec226edbcb9020f0e22320d96a1cbda0
gustavofranco26 <gustavo260123@gmail.com>
2022-12-11 06:02:47 -0500

    Adding the file that retrieves the number to each by type.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,9 +6,24 @@ Flask index file that returns the json status response
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
 def status():
     """Function for status route, return status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stats():
+    """Method stats"""
+    return jsonify(
+            {
+                "amenities": storage.count("Amenity"),
+                "cities": storage.count("City"),
+                "places": storage.count("Place"),
+                "reviews": storage.count("Review"),
+                "states": storage.count("State"),
+                "users": storage.count("User"),
+                })
